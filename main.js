647ab05cd77695a872defde2f44cd2e122f8591d
Denis Lafontant <icebox827@gmail.com>
2021-10-11 15:25:57 -0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,24 @@
+var i = 0; // Start point
+var images = [];
+var time = 3000;
+
+// Image List
+images[0] = 'img/oz-mule--212103Z 2F-worn-1-0-0-850-850_b 1.png';
+images[1] = 'img/chasse-en-inde-ashtray--300352M 01-worn-2-0-0-850-850_b 1.png';
+images[2] = 'img/avalon-iii-throw-blanket--102665M 59-worn-2-0-0-850-850_b 1.png';
+
+
+// Change Image
+function changeImg(){
+  document.slide.src = images[i];
+
+  if(i < images.length - 1){
+    i++;
+  } else {
+    i = 0;
+  }
+
+  setTimeout("changeImg()", time);
+}
+
+window.onload = changeImg;
