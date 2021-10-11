75a80dcfd83c7cf4ef150882d1b953b76814cd39
Denis Lafontant <icebox827@gmail.com>
2021-10-11 17:22:17 -0400

    Fix js linters errors

--- main.js
+++ main.js
@@ -1,24 +1,21 @@
-let i = 0; // Start point
-let images = [];
-let time = 3000;
+let i = 0;
+const images = [];
+const time = 3000;
 
-// Image List
 images[0] = 'img/oz-mule--212103Z 2F-worn-1-0-0-850-850_b 1.png';
 images[1] = 'img/chasse-en-inde-ashtray--300352M 01-worn-2-0-0-850-850_b 1.png';
 images[2] = 'img/avalon-iii-throw-blanket--102665M 59-worn-2-0-0-850-850_b 1.png';
 
-
-// Change Image
 function slider(){
   document.slide.src = images[i];
 
-  if(i < images.length - 1){
-    i++;
+  if (i < images.length - 1) {
+    i += 1;
   } else {
     i = 0;
   }
 
-  setTimeout("slider()", time);
+  setTimeout('slider()', time);
 }
 
 window.onload = slider;
