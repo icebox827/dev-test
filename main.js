04d86969121abcf2400d6a5778c2f6aff0cf39f1
Denis Lafontant <icebox827@gmail.com>
2021-10-11 17:31:55 -0400

    Fix all js linters

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ images[0] = 'img/oz-mule--212103Z 2F-worn-1-0-0-850-850_b 1.png';
 images[1] = 'img/chasse-en-inde-ashtray--300352M 01-worn-2-0-0-850-850_b 1.png';
 images[2] = 'img/avalon-iii-throw-blanket--102665M 59-worn-2-0-0-850-850_b 1.png';
 
-function slider(){
+function slider() {
   document.slide.src = images[i];
 
   if (i < images.length - 1) {
