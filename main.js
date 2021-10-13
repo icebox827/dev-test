653d4097d89bbe1ceda0f2551d8344dc84e826e5
Denis Lafontant <icebox827@gmail.com>
2021-10-13 17:35:59 -0400

    Update slider

--- main.js
+++ main.js
@@ -21,6 +21,8 @@ function showSlides(n) {
   for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";  
+  slides[slideIndex-1].style.display = "inline-block";  
   dots[slideIndex-1].className += " active";
+  slides[slideIndex].style.display = "inline-block";  
+  slides[slideIndex+1].style.display = "inline-block";  
 }
